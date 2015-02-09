1edb6eaa1699404098ccca3c2d66f75c5e202cc4
sbenavidesvazquez <sbenavidesvazquez@eniac20.danielcastelao.org>
2015-02-09 08:55:16 +0100

    hola mundo

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,17 @@
+
+package holamundo;
+
+import MiPaquete.Imprimir;
+
+
+
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("HOLA, ahora escucha la historia de mi vida de como el destino"
+                + "cambio mi movida.");
+        Imprimir.saludo();
+    }
+    
+}
